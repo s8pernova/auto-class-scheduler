51118e5d37f5c17357dc1d55c6ad2188871fca10
Aidan Hoo <achrunaway@gmail.com>
2025-12-12 22:09:06 -0500

    add little bit of the website

---
+++ ui/src/components/card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+	return (
+		<div className="bg-[#182246] h-90 rounded-[3em] text-center flex items-center justify-center hover:scale-103 transition duration-250 ease-in-out">
+			<p>Hello world</p>
+		</div>
+	);
+}
+
+export default Card;
