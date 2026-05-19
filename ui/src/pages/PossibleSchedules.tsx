ad9355ef010c1a0c62401da605aa9b40bb8fb59c
Aidan Hoo <achrunaway@gmail.com>
2026-05-19 00:36:52 -0400

    refactor: correct theme color mappings, remove Card component, and update PossibleSchedules layout

--- ui/src/pages/PossibleSchedules.tsx
+++ ui/src/pages/PossibleSchedules.tsx
@@ -1,5 +1,18 @@
+import Card from "@/components/Card"
+
 function PossibleSchedules() {
-    return <div>Possible Schedules Page</div>;
+    return (
+        <div className="flex p-[20px] h-screen">
+            <aside className="w-[22%] text-text">
+                <h2>Hello world</h2>
+                <h2>Hello world</h2>
+                <h2>Hello world</h2>
+            </aside>
+            <main className="bg-surface w-full h-full rounded-[10px] grid grid-cols-6 grid-rows-3">
+                <Card />
+            </main>
+        </div>
+    );
 }
 
 export default PossibleSchedules;
